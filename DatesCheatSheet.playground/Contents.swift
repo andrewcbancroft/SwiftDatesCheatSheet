b951b7e30fa63020f6484de63424f2126bc2e587
MDWdev <mwebdesign@icloud.com>
2017-03-17 12:41:09 -0400

    Updated to Swift 3

--- DatesCheatSheet.playground/Contents.swift
+++ DatesCheatSheet.playground/Contents.swift
@@ -1,31 +1,33 @@
 import UIKit
 
-/*: 
-# Overview
-This is a cheat sheet for working with dates, date formatters, and date components.
-
-*/
+/*:
+ # Overview
+ This is a cheat sheet for working with dates, date formatters, and date components.
+ Updated to Swift 3 - iOS 10 by MDWdev (Melissa Webster)
+ on March 17, 2017
+ 
+ */
 
 //: # Today's Date
 
-let today = NSDate()
-let thisTimeTomrrow = NSDate(timeIntervalSinceNow: 86400)
-let thisTimeYesterday = NSDate(timeIntervalSinceNow: -86400)
+let today = Date()
+let thisTimeTomorrow = Date(timeIntervalSinceNow: 86400)
+let thisTimeYesterday = Date(timeIntervalSinceNow: -86400)
 
-/*: 
-# Date Formatting
-Working with NSDateFormatter to convert dates to stings and vice-versa
-*/
+/*:
+ # Date Formatting
+ Working with NSDateFormatter to convert dates to stings and vice-versa
+ */
 
 //: ## Converting dates to strings
-let dateFormatter = NSDateFormatter()
+let dateFormatter = DateFormatter()
 dateFormatter.dateFormat = "yyyy-MM-dd"
 
-print(dateFormatter.stringFromDate(today))
+print(dateFormatter.string(from: today))
 
 dateFormatter.dateFormat = "MMM dd, yyyy"
 
-print(dateFormatter.stringFromDate(today))
+print(dateFormatter.string(from: today))
 
 
 //: ## Converting strings to dates
@@ -34,22 +36,22 @@ print(dateFormatter.stringFromDate(today))
 
 dateFormatter.dateFormat = "yyyy-MM-dd"
 var stringFromApi = "2016-01-01"
-var date = dateFormatter.dateFromString(stringFromApi)
+var date = dateFormatter.date(from: stringFromApi)
 
 stringFromApi = "May 26, 2016"
-date = dateFormatter.dateFromString(stringFromApi)
+date = dateFormatter.date(from: stringFromApi)
 
 /* Notice how the format set to the date formatter (yyyy-MM-dd) differs from the format that came back from the API (MMM dd, yyyy), which resulted in `date` being nil.
-*/
+ */
 
 /*:
-# Date Components
-Working with NSCalendar and NSCalendarUnit to work with components of dates.
-*/
+ # Date Components
+ Working with NSCalendar and NSCalendarUnit to work with components of dates.
+ */
 
 //: ## Getting date components
-var calendarUnitFlags: NSCalendarUnit = [.Year, .Month, .Day, .Hour, .Minute, .Second]
-var dateComponents = NSCalendar.currentCalendar().components(calendarUnitFlags, fromDate: NSDate())
+//var calendarUnitFlags: NSCalendar.Unit = [.year, .month, .day, .hour, .minute, .second]
+var dateComponents = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: Date())
 dateComponents.year
 dateComponents.month
 dateComponents.day
@@ -58,26 +60,26 @@ dateComponents.minute
 dateComponents.second
 
 // Observe how leaving out .Hour, .Minute, and .Second affects the values of those components of a date:
-calendarUnitFlags = [.Year, .Month, .Day]
-dateComponents = NSCalendar.currentCalendar().components(calendarUnitFlags, fromDate: NSDate())
-dateComponents.year
-dateComponents.month
-dateComponents.day
-dateComponents.hour
-dateComponents.minute
-dateComponents.second
+var dateComponents2 = Calendar.current.dateComponents([.year, .month, .day], from: Date())
+dateComponents2.year
+dateComponents2.month
+dateComponents2.day
+dateComponents2.hour
+dateComponents2.minute
+dateComponents2.second
 
 //: ## Setting date component values
 
-dateComponents.year = dateComponents.year + 1
-dateComponents.month = 1
-dateComponents.day = 1
-dateComponents.hour = 0
-dateComponents.minute = 0
-dateComponents.second = 0
+dateComponents2.year = dateComponents2.year! + 1
+dateComponents2.month = 1
+dateComponents2.day = 1
+dateComponents2.hour = 0
+dateComponents2.minute = 0
+dateComponents2.second = 0
 
 //: ## Creating dates from components
-let newDate = NSCalendar.currentCalendar().dateFromComponents(dateComponents)
+//let newDate = NSCalendar.currentCalendar().dateFromComponents(dateComponents2)
+let newDate = Calendar.current.date(from: dateComponents2)
 
 
 
